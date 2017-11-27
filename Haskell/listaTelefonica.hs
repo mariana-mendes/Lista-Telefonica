e1be81af24a017937863d2ba7b3d0078e8c2946a
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2017-11-27 16:54:40 -0300

    primeiro menu faltando o utf-8

--- Haskell/listaTelefonica.hs
+++ Haskell/listaTelefonica.hs
@@ -1,1 +1,20 @@
-# Lista Telefonica 
+
+
+printMenu:: IO() 
+printMenu = do
+	print "opcoes:"
+	print "1. Adicionar contato."
+	print "2. Listar contatos."
+	print "3. Busca contato."
+	print "4. Listar por Grupo."
+	print "5. Listar contatos ordenados"
+	print "6. Chamada de Emergência"
+	print "7. Sair"
+	print "Digite sua opção: "
+
+
+main::IO()
+main = do
+	print "----- Lista Telefonica -----"
+	printMenu
+	opcao <- getLine
