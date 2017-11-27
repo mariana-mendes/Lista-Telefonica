3bd085fa948b2f12dfa750fd406a2eb88521c534
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2017-11-27 17:11:21 -0300

    opcoes do menu com funcionalidades que precisa fazer

--- Haskell/listaTelefonica.hs
+++ Haskell/listaTelefonica.hs
@@ -12,9 +12,53 @@ printMenu = do
 	print "7. Sair"
 	print "Digite sua opção: "
 
+adicionaContato:: IO()
+adicionaContato = do
+	print "precisa fazer"
 
+listaContato:: IO()
+listaContato = do
+	print "precisa fazer"
+
+buscaContato:: IO()
+buscaContato = do
+	print "precisa fazer"
+
+listaGrupo:: IO()
+listaGrupo = do
+	print "precisa fazer"
+
+chamadaEmergencia:: IO()
+chamadaEmergencia = do
+	print "precisa fazer"
+
+listaContatosOrdenados:: IO()
+listaContatosOrdenados = do
+	print "precisa fazer"
+	
+sair:: IO()
+sair = do
+	print "precisa fazer"
+
+opcaoInvalida:: IO()
+opcaoInvalida = do
+	print "precisa fazer"	
+
+acoesMenu:: String -> IO()
+acoesMenu opcao
+	|opcao == "1" = adicionaContato 
+	|opcao == "2" = listaContato 
+	|opcao == "3" = buscaContato 
+	|opcao == "4" = listaGrupo
+	|opcao == "5" = listaContatosOrdenados
+	|opcao == "6" = chamadaEmergencia
+	|opcao == "7" = sair
+	|otherwise = opcaoInvalida
 main::IO()
 main = do
 	print "----- Lista Telefonica -----"
 	printMenu
 	opcao <- getLine
+	acoesMenu opcao
+	main 
+	
