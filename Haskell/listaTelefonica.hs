13b298dafadbf0dfe8b10e0c7d65a232d32691ed
matheusmaciell <matheus.sousa@ccc.ufcg.edu.br>
2017-12-14 16:16:05 -0300

    mudanca dos prints e sei q la

--- Haskell/listaTelefonica.hs
+++ Haskell/listaTelefonica.hs
@@ -2,11 +2,16 @@ salvaContato :: String -> String -> IO ()
 salvaContato nome numero = do
 			appendFile "lista.txt" (nome++ "," ++ numero ++ "," ++ "0" ++ "\n")
 		
+	
+	
+	
 			
 listaContatos :: IO()
 listaContatos = do
 	x <- readFile "lista.txt"
 	print x
+
+
 
 promptLine :: String -> IO String
 promptLine prompt = do
@@ -17,15 +22,15 @@ promptLine prompt = do
 
 printMenu:: IO() 
 printMenu = do
-	print "opcoes:"
-	print "1. Adicionar contato."
-	print "2. Listar contatos."
-	print "3. Busca contato."
-	print "4. Listar por Grupo."
-	print "5. Listar contatos ordenados"
-	print "6. Chamada de Emergência"
-	print "7. Sair"
-	print "Digite sua opção: "
+	putStrLn "opcoes:"
+	putStrLn "1. Adicionar contato."
+	putStrLn "2. Listar contatos."
+	putStrLn "3. Busca contato."
+	putStrLn "4. Listar por Grupo."
+	putStrLn "5. Listar contatos ordenados"
+	putStrLn "6. Chamada de Emergência"
+	putStrLn "7. Sair"
+	putStrLn "Digite sua opção: "
 
 
 
@@ -34,16 +39,16 @@ main = do
 	opcao <-  getLine
 	 
 	if((read opcao) == 1) then do
-		print "Digite o nome do contato"
+		putStrLn "Digite o nome do contato"
  		nome <- getLine
- 		print "Digite o numero do contato"
+ 		putStrLn "Digite o numero do contato"
  		numero <- getLine
 		salvaContato nome numero
 		
 	else if ((read opcao == 2)) then do
 		listaContatos
-		else
-			print "a"
-	
+	else
+		putStrLn "Opcao inválida, tente novamente."
+	main 
 
 
